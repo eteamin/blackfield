1408c899fd7c563efa21cb5e291d010ee415a429
Amin Etesamian <aminetesamian1371@gmail.com>
2016-11-15 15:20:37 +0330

    nth

--- blackfield/main.py
+++ blackfield/main.py
@@ -4,13 +4,23 @@ from queue import Queue, Empty
 from threading import Thread
 
 from kivy.app import App
+from kivy.core.text import Label
+from kivy.core.window import Window
 from kivy.uix.image import Image
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.widget import Widget
 from rfid.main import Driver
 
-from blackfield.data_access import select
-from blackfield.variables import DB_FILE, BACKGROUND
+from blackfield.data_access.select import select
+from blackfield.model import Person
+from blackfield.variables import DB_FILE, BACKGROUND, SERIAL_PATH
 
+
+def read_cart():
+    # cart_num = read()
+    # carts.put(cart_num)
+    pass
 
 screen_manager = ScreenManager()
 CART_INSERT_TIMEOUT = 1
@@ -18,35 +28,46 @@ carts = Queue()
 db_connection = sqlite3.connect(DB_FILE)
 cursor = db_connection.cursor()
 person_to_view = None
+driver = Driver(SERIAL_PATH)
+rfid_thread = Thread(name='RFID', target=read_cart, daemon=True)
+rfid_thread.start()
 
 
 class MainScreen(Screen):
 
     def __init__(self):
         super(MainScreen, self).__init__()
-        rfid_thread = Thread(name='RFID', target=self.read_cart, daemon=True)
-        rfid_thread.start()
-
         background = Image(source=BACKGROUND)
         self.add_widget(background)
-
-        try:
-            cart_num = carts.get(timeout=0.1)
-            person = select(cursor, cart_num)
-            global person_to_view
-            person_to_view = person
-            screen_manager.switch_to(ImageScreen())
-        except Empty:
-            time.sleep(0.1)
-
-    def read_cart(self):
-        cart_num = read()
-        carts.put(cart_num)
+        while True:
+            try:
+                cart_num = carts.get(timeout=0.5)
+                person = select(cursor, cart_num)
+                assert isinstance(person, Person)
+                global person_to_view
+                person_to_view = person
+                self.add_widget(PersonContainer())
+            except (Empty, Person):
+                time.sleep(0.1)
+                continue
 
 
-class ImageScreen(Screen):
+class PersonContainer(Widget):
     def __init__(self):
-        super(ImageScreen, self).__init__()
+        super(PersonContainer, self).__init__()
+        container = RelativeLayout()
+        image = Image(source=person_to_view.image)
+        title = Label(text='Sign Up', pos_hint={'center_x': 0.5, 'center_y': 0.9})
+        container.add_widget(title)
+
+        # password_input = TextInput(
+        #     hint_text='Password',
+        #     size_hint=(None, None),
+        #     size=(Window.width / 2, Window.height / 20),
+        #     pos_hint={'center_x': 0.5, 'center_y': 0.6},
+        #     password=True
+        # )
+        # container.add_widget(password_input)
 
 
 class BlackField(App):
--- blackfield/variables.py
+++ blackfield/variables.py
@@ -7,3 +7,4 @@ DB_FILE = path.abspath(path.join(path.dirname(orm.__file__), '..', 'people.db'))
 DB_TEST_FILE = path.abspath(path.join(path.dirname(orm.__file__), '..', 'people.db.test'))
 TEST_AVATAR = path.abspath(path.join(path.dirname(orm.__file__), 'tests', 'stuff', 'avatar.jpg'))
 BACKGROUND = path.abspath(path.join(path.dirname(orm.__file__), '..', 'stuff', 'background.jpg'))
+SERIAL_PATH = '/dev/ttyUSB0'
